551f9a560e5a7f493ad7ce399ad5b579cd1256b0
KofuCodes <95885468+KofuCodes@users.noreply.github.com>
2024-01-19 10:34:45 -0500

    UPDATE

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro;
 
 public class Gun : MonoBehaviour
 {
@@ -6,12 +7,21 @@ public class Gun : MonoBehaviour
     public float range = 100f;
     public float fireRate = 5f;
     public float impactForce = 30f;
+    public int magAmmo = 8;
+    public int reloadTime = 2;
 
+    public GameObject ammoCount;
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
     public GameObject impactEffect;
 
     private float nextTimeToFire = 0;
+    private int ammo = 0;
+
+    void Start()
+    {
+        ammo = magAmmo;
+    }
 
     // Update is called once per frame
     void Update()
@@ -19,7 +29,19 @@ public class Gun : MonoBehaviour
         if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
             nextTimeToFire = Time.time + 1f / fireRate;
-            Shoot();
+            if (ammo > 0)
+            {
+                ammo--;
+                ammoCount.GetComponent<TMP_Text>().text = $"{ammo}";
+                nextTimeToFire = Time.time + 1f / fireRate;
+                Shoot();
+            }
+        }
+
+        if (Input.GetKey(KeyCode.R) && ammo < 12)
+        {
+            ammo = magAmmo;
+            ammoCount.GetComponent<TMP_Text>().text = $"{ammo}";
         }
     }
 
